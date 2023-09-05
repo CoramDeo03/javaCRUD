bd913eb5d0c20358884ed2ea2eceb370086f3de4
CoramDeo03 <kajonman0616@gmail.com>
2023-09-05 23:17:06 +0900

    9/5 coding finish

--- src/main/java/crud/Main.java
+++ src/main/java/crud/Main.java
@@ -1,5 +1,15 @@
 package crud;
-
+import crud.word.WordManager;
 public class Main {
+    public static void main(String[] args) {
 
+        Main myMain = new Main();
+
+        myMain.run(args);
+    }
+
+    private void run(String[] args) {
+        WordManager wordmanager1 = new WordManager();
+        wordmanager1.Menu();
+    }
 }
